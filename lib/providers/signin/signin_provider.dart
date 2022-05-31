892be03f83346bda876f442b15c9542cf7525a43
hyuem <uhh0701@gmail.com>
2022-05-31 15:13:33 +0900

    ch 89. SigninState, SigninProvider ChangeNotifier

---
+++ lib/providers/signin/signin_provider.dart
@@ -0,0 +1,35 @@
+import 'package:fb_auth_provider/providers/signin/signin_state.dart';
+import 'package:fb_auth_provider/repositories/auth_repository.dart';
+import 'package:fb_auth_provider/models/custom_error.dart';
+import 'package:flutter/material.dart';
+
+class SigninProvider with ChangeNotifier {
+  SigninState _state = SigninState.initial();
+  SigninState get state => _state;
+
+  final AuthRepository authRepository;
+  SigninProvider({
+    required this.authRepository,
+  });
+
+  Future<void> signIn({
+    required String email,
+    required String password,
+  }) async {
+    _state = _state.copyWith(signinStatus: SigninStatus.submitting);
+    notifyListeners();
+
+    try {
+      await authRepository.signin(email: email, password: password);
+      _state = _state.copyWith(signinStatus: SigninStatus.success);
+      notifyListeners();
+    } on CustomError catch (e) {
+      _state = _state.copyWith(
+        signinStatus: SigninStatus.error,
+        error: e,
+      );
+      notifyListeners();
+      rethrow;
+    }
+  }
+}
